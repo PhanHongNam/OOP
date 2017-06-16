5f4873b7b458fdecf0c2cb07d5cb6acf8991715d
Phan Hồng Nam <idlogin97@gmail.com>
2017-06-16 18:42:13 +0700

    Update De_2_OOP.cpp
    fix

--- De_2_OOP.cpp
+++ De_2_OOP.cpp
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<string.h>
 #include<iomanip>
@@ -7,7 +8,7 @@ using namespace std;
 
 class Diem
 {
-public:
+private:
     double toan, ly, hoa;
 public:
     Diem()
@@ -44,6 +45,15 @@ public:
     {
         return hoa;
     }
+
+    double getDTB()
+    {
+        double dtb = (toan + ly + hoa)/3;
+        dtb = (int)(dtb * 100);
+        dtb = dtb + 5;
+        dtb = dtb / 100;
+        return dtb;
+    }
 };
 
 class Student
@@ -67,22 +77,6 @@ public:
         diemHocTap = obj.diemHocTap;
     }
 
-    /*void input()
-    {
-        cout << "Ten sinh vien: ";
-        fflush(stdin); gets(ten);
-        cout << "Nam sinh: ";
-        cin >> namSinh;
-        cout << "Diem tong ket 3 mon" << endl;
-        cout << "\tToan: ";
-        cin >> diemHocTap.toan;
-        cout << "\tLy: ";
-        cin >> diemHocTap.ly;
-        cout << "\tHoa: ";
-        cin >> diemHocTap.hoa;
-
-    }*/
-
     void inputList(Student s[], int n)
     {
         for(int i = 0; i < n; i++)
@@ -92,16 +86,10 @@ public:
         }
     }
 
-    /*void output()
-    {
-        cout << setw(25) << ten << setw(13) << namSinh;
-        cout << setw(6) << diemHocTap.toan << setw(6) << diemHocTap.ly << setw(6) << diemHocTap.hoa << endl;
-    }*/
-
     void outputList(Student s[], int n)
     {
         cout << setw(5) << "STT" << setw(25) << "Ten Sinh Vien" << setw(13) << "Nam Sinh";
-        cout << setw(18) << "Diem Tong Ket" << endl;
+        cout << setw(18) << "Diem Tong Ket" << setw(6) << "DTB" << endl;
         cout << setw(43) << "" << setw(6) << "Toan" << setw(6) << "Ly" << setw(6) << "Hoa" << endl;
         for (int i = 0; i < n; i++)
         {
@@ -112,12 +100,12 @@ public:
 
     double maxDTB(Student s[], int n)
     {
-        double max = (s[0].diemHocTap.toan + s[0].diemHocTap.ly + s[0].diemHocTap.hoa) / 3;
+        double max = s[0].diemHocTap.getDTB();
         for (int i = 1; i < n; i++)
         {
-            if (max < ((s[i].diemHocTap.toan + s[i].diemHocTap.ly + s[i].diemHocTap.hoa) / 3))
+            if (max < s[i].diemHocTap.getDTB())
             {
-                max = (s[i].diemHocTap.toan + s[i].diemHocTap.ly + s[i].diemHocTap.hoa) / 3;
+                max = s[i].diemHocTap.getDTB();
             }
         }
         return max;
@@ -126,11 +114,11 @@ public:
     void showMaxDTB(Student s[], int n)
     {
         cout << setw(5) << "STT" << setw(25) << "Ten Sinh Vien" << setw(13) << "Nam Sinh";
-        cout << setw(18) << "Diem Tong Ket" << endl;
+        cout << setw(18) << "Diem Tong Ket" << setw(6) << "DTB" << endl;
         cout << setw(43) << "" << setw(6) << "Toan" << setw(6) << "Ly" << setw(6) << "Hoa" << endl;
         for (int i = 0; i < n; i++)
         {
-            if ((s[i].diemHocTap.toan + s[i].diemHocTap.ly + s[i].diemHocTap.hoa) / 3 == maxDTB(s,n))
+            if (s[i].diemHocTap.getDTB() == maxDTB(s,n))
             {
                 cout << setw(5) << i + 1;
                 cout << s[i];
@@ -140,25 +128,30 @@ public:
 
     friend istream& operator>>(istream &is, Student &s)
     {
+        double d;
         cout << "Ten sinh vien: ";
         fflush(stdin); gets(s.ten);
         cout << "Nam sinh: ";
         is >> s.namSinh;
         cout << "Diem tong ket 3 mon" << endl;
         cout << "\tToan: ";
-        is >> s.diemHocTap.toan;
+        cin >> d;
+        s.diemHocTap.setToan(d);
         cout << "\tLy: ";
-        is >> s.diemHocTap.ly;
+        cin >> d;
+        s.diemHocTap.setLy(d);
         cout << "\tHoa: ";
-        is >> s.diemHocTap.hoa;
+        cin >> d;
+        s.diemHocTap.setHoa(d);
 
         return is;
     }
 
-    friend ostream& operator<<(ostream &os, const Student &s)
+    friend ostream& operator<<(ostream &os, Student &s)
     {
         os << setw(25) << s.ten << setw(13) << s.namSinh;
-        os << setw(6) << s.diemHocTap.toan << setw(6) << s.diemHocTap.ly << setw(6) << s.diemHocTap.hoa << endl;
+        os << setw(6) << s.diemHocTap.getToan() << setw(6) << s.diemHocTap.getLy() << setw(6) << s.diemHocTap.getHoa();
+        os << setw(6) << s.diemHocTap.getDTB() << endl;
 
         return os;
     }
